5a9682dcf3702b81021f5afc0d2b0b8ac8f5a350
rajnandiniini <rajnandinisinghsolanki4@gmail.com>
2026-06-19 14:02:02 +0530

    Sprint 1 Day 02 - Excel Loader and Normaliser

--- tests/etl/test_normalise.py
+++ tests/etl/test_normalise.py
@@ -33,6 +33,36 @@ def test_year_9():
 def test_year_10():
     assert normalize_year("FY2018") == 2018
 
+def test_year_11():
+    assert normalize_year("Jan 2010") == 2010
+
+def test_year_12():
+    assert normalize_year("Feb 2011") == 2011
+
+def test_year_13():
+    assert normalize_year("Apr 2018") == 2018
+
+def test_year_14():
+    assert normalize_year("May 2019") == 2019
+
+def test_year_15():
+    assert normalize_year("Jun 2020") == 2020
+
+def test_year_16():
+    assert normalize_year("Jul 2021") == 2021
+
+def test_year_17():
+    assert normalize_year("Aug 2022") == 2022
+
+def test_year_18():
+    assert normalize_year("Sep 2023") == 2023
+
+def test_year_19():
+    assert normalize_year("Oct 2024") == 2024
+
+def test_year_20():
+    assert normalize_year("Nov 2025") == 2025
+
 def test_year_none():
     assert normalize_year(None) is None
 
@@ -56,6 +86,30 @@ def test_ticker_5():
 
 def test_ticker_6():
     assert normalize_ticker("RELIANCE") == "RELIANCE"
+
+def test_ticker_8():
+    assert normalize_ticker("INFY") == "INFY"
+
+def test_ticker_9():
+    assert normalize_ticker("infy") == "INFY"
+
+def test_ticker_10():
+    assert normalize_ticker("SBI") == "SBI"
+
+def test_ticker_11():
+    assert normalize_ticker("sbi ") == "SBI"
+
+def test_ticker_12():
+    assert normalize_ticker("HCL-TECH") == "HCLTECH"
+
+def test_ticker_13():
+    assert normalize_ticker("LT.NS") == "LTNS"
+
+def test_ticker_14():
+    assert normalize_ticker(" ITC ") == "ITC"
+
+def test_ticker_15():
+    assert normalize_ticker("ASIAN-PAINTS") == "ASIANPAINTS"
 
 def test_ticker_none():
     assert normalize_ticker(None) is None
